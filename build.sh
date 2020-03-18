fe91276cf90e9d4727ea4b1c04c682096916a64f
Rafael Ferreira da Silva <rafsilva@isi.edu>
2020-03-18 15:08:00 -0700

    enabling workflow execution parallelism simulator

--- build.sh
+++ build.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 # build simulators
+echo "Compiling simulators"
 cd simulators/networking_fundamentals && ./build.sh && cd ../..
 cd simulators/multi_core_computing && ./build.sh && cd ../..
 cd simulators/workflow_execution_fundamentals && ./build.sh && cd ../..
 cd simulators/workflow_execution_data_locality && ./build.sh && cd ../..
-# cd simulators/workflow_execution_parallelism && ./build.sh && cd ../..
+cd simulators/workflow_execution_parallelism && ./build.sh && cd ../..
 cd simulators/io_operations && ./build.sh && cd ../..
 
 # copy dashboard scripts into web application
+echo "Copying WRENCH dashboard scripts into web application"
 cp -R /usr/local/wrench/dashboard web/public/sims/scripts/
 
 # bundle install jekyll application
+echo "Installing Jekyll application"
 cd web && sudo bundle install && cd ..
