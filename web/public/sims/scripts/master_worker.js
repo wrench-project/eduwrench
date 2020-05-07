f210526999f4cef5dee4a69a12f76a5e8c83af7d
Rafael Ferreira da Silva <rafsilva@isi.edu>
2020-05-07 12:22:16 -0700

    fixing issues with graphs due to modularization

--- web/public/sims/scripts/master_worker.js
+++ web/public/sims/scripts/master_worker.js
@@ -38,10 +38,8 @@ $(function () {
                 // console.log(response.task_data.workflow_execution.tasks);
 
                 let prepared_data = prepareData(response.task_data.workflow_execution.tasks);
-                generateGraph(prepared_data, "workflow-execution-chart", "taskView", 900, 500);
-                generateHostUtilizationGraph(prepared_data, "host-utilization-chart", "host-utilization-chart-tooltip",
-                    "host-utilization-chart-tooltip-task-id", "host-utilization-chart-tooltip-compute-time",
-                    900, 300);
+                generateGraph(prepared_data, "taskView", 900, 500);
+                generateHostUtilizationGraph(prepared_data, 900, 300, 60);
                 populateWorkflowTaskDataTable(prepared_data, "task-details-table", "task-details-table-body",
                     "task-details-table-td");
             }
