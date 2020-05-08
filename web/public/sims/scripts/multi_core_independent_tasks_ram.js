09cccc118af8c5ece359e77f418bef97e8762ef9
Henri Casanova <henricasanova@gmail.com>
2020-05-07 16:03:27 -1000

    Removed some output graphs when not necessary

--- web/public/sims/scripts/multi_core_independent_tasks_ram.js
+++ web/public/sims/scripts/multi_core_independent_tasks_ram.js
@@ -158,8 +158,8 @@ $(function () {
                 let prepared_data = prepareData(response.task_data.workflow_execution.tasks);
                 generateGraph(prepared_data, "taskView", 900, 500);
                 generateHostUtilizationGraph(prepared_data, 900, 300, 60);
-                populateWorkflowTaskDataTable(prepared_data, "task-details-table", "task-details-table-body",
-                    "task-details-table-td");
+                // populateWorkflowTaskDataTable(prepared_data, "task-details-table", "task-details-table-body",
+                //     "task-details-table-td");
             }
         });
     });
