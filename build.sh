9ad7cc07668a3e37070cbb1f5858ffbcdbb367a5
tabithasylee <tabitha.see.ya.lee@vanderbilt.edu>
2020-06-17 11:57:01 -0500

    Removing accidental changes

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+#  Usage  ./build.sh [-jX]
+#  Example: ./build.sh -j10 (make will run with 10 threads)
+#      
+
+# Get a numthreads argument (for parallel compilation)
+# example: 4
+if [ -z $1 ]
+then
+    makedashjarg="-j1"
+else
+    makedashjarg=$1
+fi
+
+# build simulators
+echo "Compiling simulators"
+for  dir in `ls simulators/`; do 
+    cd simulators/$dir && ./build.sh $makedashjarg && cd ../..
+done
+
+
+# copy dashboard scripts into web application
+echo "Copying WRENCH dashboard scripts into web application"
+ln -s /usr/local/wrench/dashboard web/public/sims/scripts/dashboard
+
+# bundle install jekyll application
+echo "Installing Jekyll application"
+cd web && sudo bundle install && cd ..
