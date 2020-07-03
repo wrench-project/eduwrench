c1d751cf866822745a80b5334d66c7a9174300d8
Rafael Ferreira da Silva <rafsilva@isi.edu>
2020-07-03 15:36:41 -0700

    edits to workflow distributed execution

--- web/public/sims/scripts/workflow_distributed.js
+++ web/public/sims/scripts/workflow_distributed.js
@@ -51,7 +51,6 @@ $(function () {
         }
     });
 
-
     $('#simulator-form').on('submit', function (event) {
         // we don't want the page reloading, so things look dynamic (this will be nice when we use d3's transitions)
         event.preventDefault();
@@ -93,11 +92,11 @@ $(function () {
                 // generateGanttChart(executionData);
                 generateHostUtilizationChart(executionData);
 
-                // let prepared_data = prepareData(response.task_data.workflow_execution.tasks);
+                let prepared_data = prepareData(response.task_data.workflow_execution.tasks);
                 // generateGraph(prepared_data, "taskView", 900, 500);
                 // generateHostUtilizationGraph(prepared_data, 900, 300, 60);
-                // populateWorkflowTaskDataTable(prepared_data, "task-details-table", "task-details-table-body",
-                //     "task-details-table-td");
+                populateWorkflowTaskDataTable(prepared_data, "task-details-table", "task-details-table-body",
+                    "task-details-table-td");
             }
         });
     });
