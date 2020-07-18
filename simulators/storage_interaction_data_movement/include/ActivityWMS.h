d01f57095e3efd1182416512bfa42fa61c3892dc
Rafael Ferreira da Silva <rafsilva@isi.edu>
2020-07-17 17:44:59 -0700

    adding file registry service to simulator

--- simulators/storage_interaction_data_movement/include/ActivityWMS.h
+++ simulators/storage_interaction_data_movement/include/ActivityWMS.h
@@ -1,3 +1,12 @@
+/**
+ * Copyright (c) 2020. The WRENCH Team.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
 #ifndef ACTIVITY_WMS_H
 #define ACTIVITY_WMS_H
 
