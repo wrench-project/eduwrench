7e5a7ffdd50834cf4afd364310cca4c75480a342
Henri Casanova <henricasanova@gmail.com>
2020-09-12 11:07:33 -1000

    Increased bandwidth-meter frequency, now that bandwidth time-stamp traces are optimized

--- simulators/client_server/src/ActivityWMS.cpp
+++ simulators/client_server/src/ActivityWMS.cpp
@@ -45,7 +45,7 @@ namespace wrench {
         const auto compute_service = *(this->getAvailableComputeServices<ComputeService>().begin());
 
         // Start bandwidth meters
-        const double BANDWIDTH_METER_PERIOD = 0.05;
+        const double BANDWIDTH_METER_PERIOD = 0.01;
         std::vector<std::string> linknames;
         linknames.emplace_back("link1");
         linknames.emplace_back("link2");
