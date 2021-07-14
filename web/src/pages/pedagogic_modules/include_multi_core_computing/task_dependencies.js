ef74c9973063c9ff56742188fe94c7c2ab9e2148
Rafael Ferreira da Silva <rafsilva@isi.edu>
2021-07-13 23:31:05 -0700

    multicore: drafting task dependencies module

--- web/src/pages/pedagogic_modules/include_multi_core_computing/task_dependencies.js
+++ web/src/pages/pedagogic_modules/include_multi_core_computing/task_dependencies.js
@@ -1,14 +1,16 @@
 import React from "react"
-import { Divider, Header } from "semantic-ui-react"
+import { Divider, Header, Table } from "semantic-ui-react"
 import TeX from "@matejmazur/react-katex"
 import LearningObjectives from "../../../components/learning_objectives"
 import SimulationActivity from "../../../components/simulation_activity"
 import PracticeQuestions from "../../../components/practice_questions"
+import TaskDependencies3CoresSimulation from "./task_dependencies_3_cores_simulation"
 
 import ExampleChainDAG from "../../../images/svgs/multicore_example_chain_dag.svg"
 import ExampleCarDAG from "../../../images/svgs/multicore_example_car_dag.svg"
 import ExampleSimulatedDAG from "../../../images/svgs/multicore_example_simulated_dag.svg"
-import TaskDependencies3CoresSimulation from "./task_dependencies_3_cores_simulation"
+import PracticeQuestionDAG1 from "../../../images/svgs/multicore_practice_dag_1.svg"
+import PracticeQuestionDAG2 from "../../../images/svgs/multicore_practice_dag_2.svg"
 
 const TaskDependencies = () => {
   return (
@@ -98,7 +100,7 @@ const TaskDependencies = () => {
       <p>
         To gain hands-on experience with the task dependency concept, use the simulation app below to simulate the
         execution of the above program on a 3-core computer, where <strong>each core computes at speed 10
-        Gflop/sec</strong>. You can pick the amount of work for the “analyze” task. The execution strategy used for this
+        Gflop/sec</strong>. You can pick the amount of work for the "analyze" task. The execution strategy used for this
         execution is very simple: whenever a task can be executed (because all its parent tasks have been executed) and
         a core is (or becomes) idle, then execute that task on that core immediately. We call a task whose parents have
         all executed a <strong>ready task</strong>. The following practice questions are based on this simulation app.
@@ -209,6 +211,166 @@ const TaskDependencies = () => {
         them explicitly. Let's do so now.
       </p>
 
+      <p>
+        A first concept is that of a <strong>DAG level</strong>. A task is on level <TeX math="n" /> of the DAG if the
+        longest path from the entry task(s) to this task is of length <TeX math="n" />, <strong>where the path length is
+        measured in number of vertices traversed before reaching this task</strong>. By this definition, an entry task
+        is in level 0. Every child task of an entry task is in level 1, and so on. Formally, the level of a task is one
+        plus the maximum of the levels of its parent tasks (this is a recursive definition).
+      </p>
+
+      <p>For our example DAG in Figure 3 above, we can determine the level of each task:</p>
+
+      <Table collapsing size="small">
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>task</Table.HeaderCell>
+            <Table.HeaderCell>level</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>start</Table.Cell>
+            <Table.Cell>0</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>viz</Table.Cell>
+            <Table.Cell>1</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>analyze</Table.Cell>
+            <Table.Cell>1</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>stats</Table.Cell>
+            <Table.Cell>1</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>plot</Table.Cell>
+            <Table.Cell>2</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>summarize</Table.Cell>
+            <Table.Cell>2</Table.Cell>
+          </Table.Row>
+          <Table.Row>
+            <Table.Cell>display</Table.Cell>
+            <Table.Cell>3</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+
+      <p>
+        So we say that this DAG has four levels. Note that this does not mean that the DAG tasks must be executed level
+        by level. For instance, we could execute task "plot" (level 2) before task "analyze" (level 1).
+      </p>
+
+      <p>
+        A second concept is that of <strong>maximum level width</strong>: the maximum number of tasks in the workflow
+        levels. For instance, for our example DAG, the maximum level width is 3 because level 1 has 3 tasks (and all
+        other levels have fewer tasks). This means that using 3 cores should lead to better performance than using 2
+        cores. If we enforce that tasks are executed level-by-level, then we cannot make use of more than 3 cores.
+        Otherwise, in general, it may be possible to use more cores to gain some performance advantage. But this is not
+        the case for the example DAG in Figure 3, for which a 4th core would never be used.
+      </p>
+
+      <p>
+        A third concept is that of the <strong>critical path</strong>: the longest path in the DAG from the entry
+        task(s) to the exit task(s), where <strong>the path length is measured in task durations, including the entry
+        and the exit task(s)</strong>. No matter how many cores are used, the program cannot execute faster than the
+        length of the critical path. For instance, consider our example DAG, assuming that the "analyze" task has work
+        250 Gflop. There are three paths from "start" to "display". The length of the visualization path
+        is <TeX math="5+20+10+1 = 36" /> seconds. The length of the statistics path is <TeX math="5+40+1=46" /> seconds.
+        The length of the analysis path is <TeX math="5+25+10+1=41" /> seconds. And so the critical path is {"{"}start"
+        -> "stats" -> "display"{"}"}, of length 46 seconds. No matter how many 10 Gflop/sec cores are used to execute
+        this program, it can never run in less than 46 seconds!
+      </p>
+
+      <Divider />
+
+      <PracticeQuestions questions={[
+        {
+          key: "A.2.p4.4",
+          question: (
+            <>
+              For the DAG below, give the number of levels, the maximum level width, and the length of the critical path
+              in seconds (name and execution time are shown for each task).
+              <PracticeQuestionDAG1 />
+            </>
+          ),
+          content: (
+            <>
+              <ul>
+                <li>Number of levels: 4</li>
+                <li>Maximum level width: 3 (level 3 has 3 tasks: G, E, and F)</li>
+                <li>Length of the critical path: 30s (A 1s, D 20s, F 7s, and H 2s)</li>
+              </ul>
+            </>
+          )
+        },
+        {
+          key: "A.2.p4.5",
+          question: (
+            <>
+              For the DAG below, would it be useful to use more than 3 cores? Can the execution time be ever shorter
+              than 29 seconds? Could you modify one edge's end point to increase the DAG's maximum level width? Show
+              your work and reasoning.<br />
+              <PracticeQuestionDAG2 />
+            </>
+          ),
+          content: (
+            <>
+              Here is the set of DAG levels:
+              <Table collapsing size="small">
+                <Table.Header>
+                  <Table.Row>
+                    <Table.HeaderCell>level</Table.HeaderCell>
+                    <Table.HeaderCell>tasks</Table.HeaderCell>
+                  </Table.Row>
+                </Table.Header>
+
+                <Table.Body>
+                  <Table.Row>
+                    <Table.Cell>0</Table.Cell>
+                    <Table.Cell>A</Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>1</Table.Cell>
+                    <Table.Cell>B, C</Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>2</Table.Cell>
+                    <Table.Cell>D, E, F</Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>3</Table.Cell>
+                    <Table.Cell>G</Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>4</Table.Cell>
+                    <Table.Cell>H</Table.Cell>
+                  </Table.Row>
+                </Table.Body>
+              </Table>
+              <p>
+                It would never be useful to use more than 3 cores because the width of the DAG is 3 (level 2). The DAG's
+                critical path is {"{"}A->B->D->G->H{"}"}, which has length 28s. So yes, the execution (on 3 cores) could
+                be lower than 29s.
+              </p>
+              <p>
+                Replacing the D->G edge by a D->H edge would make the DAG’s maximum level width 4 (i.e., level 2 would
+                have 4 tasks in it).
+              </p>
+            </>
+          )
+        }
+      ]} />
+
+      <Divider />
+
+      <h2>Choosing which task to run next</h2>
+
     </>
   )
 }
@@ -216,244 +378,6 @@ const TaskDependencies = () => {
 export default TaskDependencies
 
 const text1 = `
-
-#### Practice Questions
-
-**[A.2.p4.1]**  Say we run the program with an "analyze" task that has 100
-Gflop work. What is the parallel efficiency when running the program on the
-3-core computer and when using a single analysis task? Show your work, and feel free to use
-the simulation app to help you.
-
-  <div class="ui accordion fluid">
-    <div class="title">
-      <i class="dropdown icon"></i>
-      (click to see answer)
-    </div>
-    <div markdown="1" class="ui segment content answer-frame">
-      The sequential program's execution on 1 core, *T(1)*, is simply the sum
-      of individual task execution times,
-
-      $
-      T(1) = 5 + 20 + 10 + 10 + 10 + 40 + 1 = 96 \\;\\text{sec}
-      $
-
-      The simulated execution time on our 3-core computer is:
-
-      $
-      T(3) = 46 \\;\\text{sec}
-      $
-
-      So the parallel efficiency is $E(3) = (96/46)/3 =$ **69.56%**.
-    </div>
-  </div>
-
-  <p></p>
-
-**[A.2.p4.2]** What is the number of idle core seconds when running the
-program when the "analyze" task has 300 Gflop work on our 3-core computer? Show your work.
-You can double-check your answer in simulation.
-
-  <div class="ui accordion fluid">
-    <div class="title">
-      <i class="dropdown icon"></i>
-      (click to see answer)
-    </div>
-    <div markdown="1" class="ui segment content answer-frame">
-
-      This is a very similar question as the previous one. The sequential
-      execution time is 126 seconds, and the execution time on 3 cores is still
-      46 seconds. Therefore, the number of core idle seconds is $46 \\times 3 - 126 = 12$ seconds.
-
-      We can double check this answer by counting the number of idle seconds as
-      shown in the Host Utilization graph of the simulation app.
-
-    </div>
-  </div>
-
-  <p></p>
-
-**[A.2.p4.3]** For what amount of work of the "analyze" task is the
-parallel efficiency maximized? Show your work. You could use the simulation app to "search"
-for the right answer, but that would be really tedious. Try using analysis
-and/or intuition first. This is not an easy question, as it requires careful reasoning.
-Hint: consider two cases depending on whether the critical path is the analysis path
-or the statistics path
-
-  <div class="ui accordion fluid">
-    <div class="title">
-      <i class="dropdown icon"></i>
-      (click to see answer)
-    </div>
-    <div markdown="1" class="ui segment content answer-frame">
-
-      Let's first do a purely analytical solution.
-      Let $x$ be the work of the "analyze" task in Gflop. The sequential execution
-      time is $x/10 + 86$ seconds.
-
-      The parallel execution time is a bit trickier.
-
-      The visualization path takes time $5 + 20 + 10 + 1 = 36$ seconds, which
-      is shorter than the statistics path, which takes 46 seconds. The analysis path takes time
-      $5 + x/10 + 10 + 1 = 16 + x/10$ seconds.
-
-      So, we have two cases: If $16 + x/10 \\leq 46$, that is, if $x \\leq 300$,
-      the critical path is the analysis path, otherwise the critical
-      path is the statistics path. So let's examine both cases:
-
-      - $x \\leq 300$: the parallel execution time is 46 seconds, and so the parallel efficiency
-      is equal to $((x/10 + 86) / 46) / 3$. This is maximized for $x = 300$, and is then equal
-      to 84.05%.
-
-      - $x \\geq 300$: the parallel execution time is 16 + x/10, and so the parallel efficiency
-      is equal to $((x/10 + 86) / (16 + x/10)) / 3$. This is a decreasing function on the [300, infinity] domain,
-      and so on that domain it is maximized for $x = 300$.
-
-      The final answer is thus 300 Gflop.
-
-      The above is quite formal, but we could have given a purely
-      common-sense answer.
-      The parallel efficiency is maximized when all three paths take
-      time as close as possible as the longest such path, so as have
-      cores working as much as possible. This is the same *load balancing*
-      idea that we have seen in the
-      [Parallelism tab]({{ site.baseurl }}/pedagogic_modules/pdcc/multi_core_computing/#/parallelism)
-      for independent tasks!
-      This is achieved when the analysis
-      path and the statistics path are equal (nothing can be done about the
-      visualization path), that is, when $x = 300$.
-
-      For $x = 300$ the efficiency is 84.05%, which is the best this program can ever achieve.
-
-    </div>
-  </div>
-
-  <p></p>
-
-### Levels, Width, Critical Path
-
-In the previous section, and the practice questions, we touched upon some
-fundamental concepts without naming them explicitly. Let's do so now.
-
-A first concept is that of a **DAG level**. A task is on level $n$ of the
-DAG if the longest path from the entry task(s) to this task is of length
-$n$, **where the path length is measured in number of vertices traversed before
-reaching this task**. By this definition, an entry task is
-in level 0. Every child task of an entry task is in level 1, and so on.
-Formally, the level of a task is one plus the maximum of the levels of its
-parent tasks (this is a recursive definition).
-
-For our example DAG in Figure 3 above, we can determine the level of each task:
-
-| task | level |
-|------|-------|
-| start| 0|
-| viz | 1|
-|analyze |1|
-|stats| 1|
-|plot | 2|
-|summarize |2|
-|display|3|
-|------|-------|
-
-So we say that this DAG has four levels. Note that this does not mean
-that the DAG tasks must be executed level by level. For instance, we could execute
-task "plot" (level 2) before task "analyze" (level 1).
-
-A second concept is that of **maximum level width**: the maximum
-number of tasks in the workflow levels. For instance, for our example DAG, the
-maximum level width is 3
-because level 1 has 3 tasks (and all other levels have fewer tasks).
-This means that using 3 cores should lead to better performance than using 2 cores.
-If we enforce that tasks are executed level-by-level, then we cannot make use of
-more than 3 cores. Otherwise, in general, it may be possible to use more cores
-to gain some performance advantage. But this is not the case for the example DAG
-in Figure 3, for which a 4th core would never be used.
-
-A third concept is that of the **critical path**:
-the longest path in the DAG from the entry task(s) to the exit
-task(s), where **the path length is measured in task durations, including the
-entry and the exit task(s)**.
-No matter how many cores are used, the program cannot execute faster than
-the length of the critical path. For instance, consider our example
-DAG, assuming that the "analyze" task has work 250 Gflop. There are three paths
-from "start" to "display". The length of the visualization path is
-5+20+10+1 = 36 seconds. The length of the statistics path is 5+40+1=46 seconds. The
-length of the analysis path is 5+25+10+1=41 seconds. And so the critical path
-is
-  {
-    "start"
-  ->
-    "stats"
-  ->
-    "display"
-  }
-, of length 46 seconds. No matter how many 10 Gflop/sec cores
-are used to execute this program, it can never run in less than 46 seconds!
-
-#### Practice Questions
-
-**[A.2.p4.4]** For the DAG below, give the number of levels, the maximum level width, and the
-length of the critical path in seconds (name and execution time are shown for each task).
-
-  <object class="figure" type="image/svg+xml"
-          data="{{ site.baseurl }}/public/img/multi_core_computing/practice_dag_1.svg">Practice Question DAG</object>
-
-  <div class="ui accordion fluid">
-    <div class="title">
-      <i class="dropdown icon"></i>
-      (click to see answer)
-    </div>
-    <div markdown="1" class="ui segment content answer-frame">
-
-      - Number of levels: 4
-      - Maximum level width: 3 (level 3 has 3 tasks: G, E, and F)
-      - Length of the critical path: 30s (A 1s, D 20s, F 7s, and H 2s)
-
-    </div>
-  </div>
-
-  <p></p>
-
-
-**[A.2.p4.5]** For the DAG below, would it be useful to use more than 3
-cores? Can the execution time be ever shorter than 29 seconds? Could you
-modify one edge's end point to increase the DAG's maximum level width?
-Show your work and reasoning.
-
-  <object class="figure" type="image/svg+xml"
-          data="{{ site.baseurl }}/public/img/multi_core_computing/practice_dag_2.svg">Practice Question DAG</object>
-
-  <div class="ui accordion fluid">
-    <div class="title">
-      <i class="dropdown icon"></i>
-      (click to see answer)
-    </div>
-    <div markdown="1" class="ui segment content answer-frame">
-
-      Here is the set of DAG levels:
-
-      |------|-----|
-      | level | tasks |
-      |------|-----|
-      | 0 | A |
-      | 1 | B, C|
-      | 2 | D, E, F|
-      | 3 | G |
-      | 4 | H |
-      |---|-----|
-
-      It would never be useful to use more than 3 cores because the width of the DAG is 3 (level 2). The DAG's
-      critical path is {A->B->D->G->H}, which has length 28s. So yes, the execution (on 3 cores) could be lower than
-      29s.
-
-      Replacing the D->G edge by a D->H edge would make the DAG's maximum level width 4 (i.e., level 2 would have 4
-      tasks in it).
-    </div>
-  </div>
-
-  <p></p>
-
----
 
 ### Choosing which task to run next
 
