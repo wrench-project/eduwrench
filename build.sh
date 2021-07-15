5cc4fd4c8141c8835b6dfe904345f03d0c686e14
Rafael Ferreira da Silva <rafsilva@isi.edu>
2021-07-14 22:01:37 -0700

    updating build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,7 @@
 #
 #  Usage  ./build.sh [-jX]
 #  Example: ./build.sh -j10 (make will run with 10 threads)
-#      
-
+#
 
 # Get a numthreads argument (for parallel compilation)
 # example: 4
@@ -17,9 +16,9 @@ fi
 
 # build simulators
 echo "Compiling simulators"
-for  dir in `ls simulators/`; do 
+for  dir in `ls simulators/`; do
     echo "Building in simulator/$dir ..."
-    cd simulators/$dir 
+    cd simulators/$dir
     if [[ -f build.sh  ]]; then
         ./build.sh $makedashjarg
         if [[ $? -ne 0 ]]; then
@@ -30,10 +29,13 @@ for  dir in `ls simulators/`; do
 done
 
 
-# copy dashboard scripts into web application
-echo "Copying WRENCH dashboard scripts into web application"
-ln -s /usr/local/wrench/dashboard web/public/sims/scripts/dashboard
+#DEPRECATED copy dashboard scripts into web application
+#echo "Copying WRENCH dashboard scripts into web application"
+#ln -s /usr/local/wrench/dashboard web/public/sims/scripts/dashboard
 
 #DEPRECATED bundle install jekyll application
 #echo "Installing Jekyll application"
 #cd web && sudo bundle install && cd ..
+
+echo "Installing NPM modules"
+cd web && npm install && cd ..
