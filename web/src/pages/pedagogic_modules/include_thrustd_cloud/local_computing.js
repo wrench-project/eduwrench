7e09429d63060e0369ed11d37975e56e6fc8fa05
Derrick Luyen <46693824+derrickluyen@users.noreply.github.com>
2021-07-25 20:11:24 -1000

    refactoring same thing as previous commit

--- web/src/pages/pedagogic_modules/include_thrustd_cloud/local_computing.js
+++ web/src/pages/pedagogic_modules/include_thrustd_cloud/local_computing.js
@@ -3,7 +3,7 @@ import { Divider, Header, Table } from "semantic-ui-react"
 import TeX from "@matejmazur/react-katex"
 import LearningObjectives from "../../../components/learning_objectives"
 import IOFigure1 from "../../../images/vector_graphs/single_core/IO_figure_1.svg";
-import Thrustd_local_simulation from "./thrustd_local_simulation";
+import Thrustd_Local_Simulation from "./thrustd_local_simulation";
 import SimulationActivity from "../../../components/simulation/simulation_activity";
 
 const LocalComputing = () => {
