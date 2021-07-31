2810c908848064c5ccf450312045312d5242ecf5
Rafael Ferreira da Silva <rafsilva@isi.edu>
2021-07-30 16:35:08 -1000

    code cleanup

--- docker.sh
+++ docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "${EDUWRENCH_ENABLE_SSL}" = true ]; then
-  gatsby serve --trace -H 0.0.0.0 -p 4000 --ssl-key ssl/${EDUWRENCH_SSL_PRIVATE_KEY} --ssl-cert ssl/${EDUWRENCH_SSL_CERTIFICATE} && node /home/wrench/eduwrench/server/app.js
+  gatsby serve -H 0.0.0.0 -p 4000 --key-file ssl/${EDUWRENCH_SSL_PRIVATE_KEY} --cert-file ssl/${EDUWRENCH_SSL_CERTIFICATE} && node /home/wrench/eduwrench/server/app.js
 else
   gatsby serve -H 0.0.0.0 -p 4000 && node /home/wrench/eduwrench/server/app.js
 fi
