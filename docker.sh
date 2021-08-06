c6ac3aaf26a4d668425fd4b8da675194eacdae6e
Rafael Ferreira da Silva <rafsilva@isi.edu>
2021-08-05 18:06:30 -0700

    updating docker.sh

--- docker.sh
+++ docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "${EDUWRENCH_ENABLE_SSL}" = true ]; then
-  gatsby serve -H 0.0.0.0 -p 4000 --key-file ssl/${EDUWRENCH_SSL_PRIVATE_KEY} --cert-file ssl/${EDUWRENCH_SSL_CERTIFICATE} && node /home/wrench/eduwrench/server/app.js
+  gatsby develop -H 0.0.0.0 -p 4000 --https --key-file ssl/${EDUWRENCH_SSL_PRIVATE_KEY} --cert-file ssl/${EDUWRENCH_SSL_CERTIFICATE} && node /home/wrench/eduwrench/server/app.js
 else
   gatsby serve -H 0.0.0.0 -p 4000 && node /home/wrench/eduwrench/server/app.js
 fi
