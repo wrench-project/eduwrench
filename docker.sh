0bc5992f1f98c1b6c79bee0fa4639d800f4077c1
Henri Casanova <henric@hawaii.edu>
2021-08-06 15:12:34 -0700

    Fixed .env Fixed docker.sh

--- docker.sh
+++ docker.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# Start Node server
+cd /home/wrench/eduwrench/server
+node app.js &
+
+# Start Gatsby server
+cd /home/wrench/eduwrench/web
 if [ "${EDUWRENCH_ENABLE_SSL}" = true ]; then
-  gatsby develop -H 0.0.0.0 -p 4000 --https --key-file ssl/${EDUWRENCH_SSL_PRIVATE_KEY} --cert-file ssl/${EDUWRENCH_SSL_CERTIFICATE} && node /home/wrench/eduwrench/server/app.js
+  gatsby develop -H 0.0.0.0 -p 4000 --https --key-file ssl/${EDUWRENCH_SSL_PRIVATE_KEY} --cert-file ssl/${EDUWRENCH_SSL_CERTIFICATE}
 else
-  gatsby serve -H 0.0.0.0 -p 4000 && node /home/wrench/eduwrench/server/app.js
+  gatsby serve -H 0.0.0.0 -p 4000
 fi
+
+
