73c896808e7afd22b6a64de2a31b5173be1e1a72
Derrick Luyen <46693824+derrickluyen@users.noreply.github.com>
2021-08-09 14:03:14 -1000

    small changes

--- server/app.js
+++ server/app.js
@@ -1177,9 +1177,8 @@ app.post("/run/ci_overhead", function (req, res) {
     }
 });
 
-<<<<<<< HEAD
 // execute thrust d simulator
-app.post("/run/thrustd", authCheck, function (req, res) {
+app.post("/run/thrustd", function (req, res) {
   const PATH_PREFIX = __dirname.replace(
       "server",
       "simulators/thrustd/build/"
@@ -1237,65 +1236,30 @@ app.post("/run/thrustd", authCheck, function (req, res) {
   const SIMULATION_ARGS = [
     "/tmp/args.json"
   ].concat(LOGGING);
-  const RUN_SIMULATION_COMMAND = [EXECUTABLE].concat(SIMULATION_ARGS).join(" ");
 
-  console.log("\nRunning Simulation");
-  console.log("===================");
-  console.log("Executing command: " + RUN_SIMULATION_COMMAND);
-  var simulation_process = spawnSync(EXECUTABLE, SIMULATION_ARGS);
+  var simulation_output = launchSimulation(EXECUTABLE, SIMULATION_ARGS);
 
-  if (simulation_process.status != 0) {
-    console.log(
-        "Something went wrong with the simulation. Possibly check arguments."
-    );
-    console.log(simulation_process.stderr.toString());
-  } else {
-    var simulation_output = simulation_process.stderr.toString();
-    console.log(simulation_output);
-  
-    /**
-     * Log the user running this simulation along with the
-     * simulation parameters to the data server.
-     */
-    logData({
-      user: USERNAME,
-      email: EMAIL,
-      time: Math.round(new Date().getTime() / 1000), // unix timestamp
-      activity: "thrustd",
-      num_hosts: NUM_HOSTS,
-      pstate: PSTATE
-    });
+  if (simulation_output !== null) {
+      /**
+       * Log the user running this simulation along with the
+       * simulation parameters to the data server.
+       */
+      logData({
+          user: USERNAME,
+          email: EMAIL,
+          time: Math.round(new Date().getTime() / 1000), // unix timestamp
+          activity: "thrustd",
+          num_hosts: NUM_HOSTS,
+          pstate: PSTATE
+      });
 
-    /**
-     * The simulation output uses ansi colors and we want these colors to show up in the browser as well.
-     * Ansi up will take each line, make it into a <span> element, and edit the style so that the text color
-     * is whatever the ansi color was. Then the regular expression just adds in <br> elements so that
-     * each line of output renders on a separate line in the browser.
-     *
-     * The simulation output and the workflowtask data are sent back to the client (see public/scripts/activity_1.js)
-     */
-    var find = "</span>";
-    var re = new RegExp(find, "g");
-
-    res.json({
-      simulation_output: ansi_up
-          .ansi_to_html(simulation_output)
-          .replace(re, "<br>" + find),
-      task_data: JSON.parse(fs.readFileSync("/tmp/workflow_data.json")),
-    });
+      res.json({
+          "simulation_output": ansiUpSimulationOutput(simulation_output),
+          "task_data": JSON.parse(fs.readFileSync("/tmp/workflow_data.json")),
+      })
   }
 });
 
-function storeData(data){
-  sims
-  .add(data)
-  //.then((simulation) => {
-  //  res.status(200).json(simulation);
-  //})
-  //.catch((error) => {
-  //  res.status(500).json({ message: error.message });
-  //});
-=======
 // execute activity storage service simulation route
 app.post("/run/storage_service", function (req, res) {
     const PATH_PREFIX = __dirname.replace("server", "simulators/storage_interaction_data_movement/");
@@ -1391,9 +1355,7 @@ function storeData(data) {
     //.catch((error) => {
     //  res.status(500).json({ message: error.message });
     //});
->>>>>>> f2f115650206c1a05002c2103ed17b9d824afd79
 }
-
 
 /**
  * Log the data into the JSON file
