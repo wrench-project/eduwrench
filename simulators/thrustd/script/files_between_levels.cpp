ec7286c8a976a91c28b25f89e948292976ab1732
Henri Casanova <henric@hawaii.edu>
2022-01-25 17:11:33 -1000

    build process fix

--- simulators/thrustd/script/files_between_levels.cpp
+++ simulators/thrustd/script/files_between_levels.cpp
@@ -20,31 +20,44 @@ int main(int argc, char **argv) {
     workflow = wrench::PegasusWorkflowParser::createWorkflowFromJSON(workflow_file, reference_speed, false,
                                                                      4, 4, true);
 
-    std::vector<wrench::WorkflowFile*> files[8];
-    int numFiles[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-    int numTasks[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-    double numFlops[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+//    std::vector<wrench::WorkflowFile*> files[8];
+//    int numFiles[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+//    int numTasks[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+//    double numFlops[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+//
+//    for (auto const &t : workflow->getTasks()) {
+//        numTasks[t->getTopLevel()]++;
+//        numFlops[t->getTopLevel()] += t->getFlops();
+//        for (auto const &f : t->getInputFiles()) {
+//            files[t->getTopLevel()].push_back(f);
+//            numFiles[t->getTopLevel()]++;
+//        }
+//    }
+//
+//    double sizes[8];
+//
+//    for(int i = 0; i < 8; i++) {
+//        for (auto const &f : files[i]) {
+//            sizes[i] += f->getSize();
+//        }
+//    }
 
-    for (auto const &t : workflow->getTasks()) {
-        numTasks[t->getTopLevel()]++;
-        numFlops[t->getTopLevel()] += t->getFlops();
-        for (auto const &f : t->getInputFiles()) {
-            files[t->getTopLevel()].push_back(f);
-            numFiles[t->getTopLevel()]++;
-        }
+    std::cerr << "Total number of tasks: " << workflow->getNumberOfTasks() << "\n";
+    std::cerr << "Total number of levels: " << workflow->getNumLevels() << "\n";
+    for (int i = 0; i < workflow->getNumLevels(); i++) {
+        std::cerr << "   Level " << i << ": " << workflow->getTasksInTopLevelRange(i, i).size() << "\n";
     }
-
-    double sizes[8];
-
-    for(int i = 0; i < 8; i++) {
-        for (auto const &f : files[i]) {
-            sizes[i] += f->getSize();
-        }
+    double footprint = 0;
+    for (auto const &f : workflow->getFileMap()) {
+        footprint += f.second->getSize();
     }
+    std::cerr << "Data footprint (kB): " << footprint/(1000) << "\n";
+    std::cerr << "Data footprint (MB): " << footprint/(1000*1000) << "\n";
+    std::cerr << "Data footprint (GB): " << footprint/(1000 * 1000 * 1000) << "\n";
 
-    for (int i = 0; i < 8; i++) {
-        std::cerr << "Before level " << i << ": " << numFiles[i] << " files, total size: " << sizes[i] / 1000000 << " MB" << std::endl;
-        std::cerr << "At level " << i << ": " << numTasks[i] << " tasks, total work: " << (numFlops[i]/GFLOPS) << " GFlops" << std::endl;
-    }
+//    for (int i = 0; i < 8; i++) {
+//        std::cerr << "Before level " << i << ": " << numFiles[i] << " files, total size: " << sizes[i] / 1000000 << " MB" << std::endl;
+//        std::cerr << "At level " << i << ": " << numTasks[i] << " tasks, total work: " << (numFlops[i]/GFLOPS) << " GFlops" << std::endl;
+//    }
 }
 
