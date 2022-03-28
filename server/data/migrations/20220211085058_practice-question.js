f1326a0d7a4be59794ea45f413d13b645f3b6dff
skyler8 <skyler8@hawaii.edu>
2022-03-27 17:35:29 -1000

    database can hold user_id and query user data for practice question

--- server/data/migrations/20220211085058_practice-question.js
+++ server/data/migrations/20220211085058_practice-question.js
@@ -1,6 +1,7 @@
 
 exports.up = function(knex) {
     return knex.schema.createTable("practice_questions", tbl => {
+        tbl.integer("user_id").references("id").inTable("users").onDelete("CASCADE")
         tbl.text("question_key").notNullable().primary()
         tbl.integer("time").notNullable()
         tbl.boolean("completed").notNullable()
