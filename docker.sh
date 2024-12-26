a4e1047db12e26f9d49d0dd5e7b92ca82745c10c
Henri Casanova <henric@hawaii.edu>
2024-12-25 17:16:32 -1000

    enabling local development

--- docker.sh
+++ docker.sh
@@ -29,5 +29,5 @@ cd /home/wrench/eduwrench/web
 if [ "${EDUWRENCH_ENABLE_SSL}" = true ]; then
   gatsby develop -H 0.0.0.0 -p 4000 --https --key-file ssl/${EDUWRENCH_SSL_PRIVATE_KEY} --cert-file ssl/${EDUWRENCH_SSL_CERTIFICATE}
 else
-  gatsby serve -H 0.0.0.0 -p 4000
+  gatsby develop -H 0.0.0.0 -p 4000
 fi
