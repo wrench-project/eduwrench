4e577d02054e2d5775545018d2f839f517684884
Henri Casanova <henric@hawaii.edu>
2025-04-05 07:30:57 -1000

    fix?

--- server/docker-server.sh
+++ server/docker-server.sh
@@ -5,6 +5,7 @@ printf "\n${NC}"
 printf "${CYAN}"
 echo "Calling knex migrate:latest"
 printf "${NC}\n"
+npx knex migrate:unlock
 npx knex migrate:latest
 printf "${CYAN}"
 echo "Starting node backend server"
